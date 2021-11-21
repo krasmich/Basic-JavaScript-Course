bf316874ff4eafb6b55605f325498854ae56b14d
krasmich <kraskovmichael@gmail.com>
2021-11-21 12:37:46 +0400

    ДЗ к 7 уроку

---
+++ task3/task3.1/app.js
@@ -0,0 +1,58 @@
+'use strict';
+const texts = {
+    text1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    text2: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.',
+    text3: 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил.'
+};
+
+/* 
+1. Получите ссылку на .text, например с помощью querySelector
+2. Получите коллекцию, в которой хранятся все .nav-link, например с помощью querySelectorAll
+    2.1 Переберите полученную коллекцию, например с помощью forEach, и каждой ссылке назначьте
+    обработчик клика функцию clickHandler.
+*/
+
+let contents = document.querySelector('.text');
+let tabs = document.querySelectorAll('.nav-link');
+
+tabs.forEach(tab => tab.addEventListener('click', function clickHandler(event) {
+    changeActiveClass(event);
+    changeText(event);
+    // здесь вызывайте changeText и changeActiveClass, и передавайте им объект события
+}))
+
+/**
+ * Обработчик клика по .nav-link
+ * @param {MouseEvent} event 
+ */
+
+
+/**
+ * Эта функция должна убирать .active у предыдущего .nav-link и ставить его
+ * на тот, по которому кликнули.
+ * @param {MouseEvent} event 
+ */
+function changeActiveClass(event) {
+    tabs.forEach(function (item) {
+        item.classList.remove('active')
+    });
+    event.target.classList.add('active');
+}
+
+/**
+ * Эта фукнция должна читать текст (например через textContent) из 
+ * .nav-link по которому кликнули и в зависимости от этого в .text
+ * ставить соответствующий текст из texts.
+ * @param {MouseEvent} event 
+ */
+function changeText(event) {
+    // console.log(event.target.textContent);
+    if (event.target.textContent == 'Link 1') {
+        contents.textContent = texts.text1;
+    } else if (event.target.textContent == 'Link 2') {
+        contents.textContent = texts.text2;
+    } else if (event.target.textContent == 'Link 3') {
+        contents.textContent = texts.text3;
+    }
+
+}
